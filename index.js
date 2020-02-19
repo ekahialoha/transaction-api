1b80abd2c9d2faa335e3afd71282917d8508f1a7
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2020-02-18 16:59:39 -0800

    dotEnv added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-// ============
-// Simple application
-// ============
+// ==========================
+// Application Dependencies
+// ==========================
+require('dotenv').config();
 const express = require('express');
+
+// ====================
+// Simple application
+// ====================
 const app = express();
 const port = 3000;
+
 
 app.get('/', (req, res) => res.send('Hello :)'));
 app.listen(port, () => console.log(`APPLICATION STATUS: Listening on port ${port}`))
