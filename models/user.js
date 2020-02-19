76eafe1c713982084927b5596f9959b211602e7e
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2020-02-18 23:20:51 -0800

    start of User model/migration

---
+++ models/user.js
@@ -0,0 +1,24 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validates: { len: [4, 150] }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validates: { isEmail: true }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
