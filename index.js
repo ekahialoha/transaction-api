3b0297b345b25dab466b310a5190f97ea053da17
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2020-02-18 23:25:10 -0800

    clean up index

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
 // ===================================
 const dotEnv = require('dotenv').config();
 const express = require('express');
-const sequelize = require('sequelize');
-const routes = require('./routes/api.js');
 const apiRoutes = require('./routes/api.js');
+const db = require('./models');
 
 // ====================
 // Simple application
@@ -13,10 +12,6 @@ const apiRoutes = require('./routes/api.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// =====================
-// Database Connection
-// =====================
-const db = new sequelize(process.env.DATABASE_URI);
 
 // ========
 // Routes
@@ -25,9 +20,5 @@ app.use('/api', apiRoutes);
 
 // ==================
 // Start up server
-db.sync()
-  .then(() => {
-    console.log('Postgres/Sequelize connection successfull.');
-    app.listen(port, () => console.log(`APPLICATION STATUS: Listening on port ${port}`));
-  }).catch(() => console.log('Unable to establish Postgres/Sequelize connection.'));
 // ==================
+app.listen(port, () => console.log(`Node Server: Listening on port ${port}`));
