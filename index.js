6efe8158c4cc7c0ed1447be0f6028fe536f9b880
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2020-02-18 23:12:30 -0800

    corrections/clean up

--- index.js
+++ index.js
@@ -18,17 +18,16 @@ const port = process.env.PORT || 3000;
 // =====================
 const db = new sequelize(process.env.DATABASE_URI);
 
-// #########
+// ========
 // Routes
-// #########
-routes(app, db);
+// ========
 app.use('/api', apiRoutes);
 
-// ##################
+// ==================
 // Start up server
-// ##################
 db.sync()
   .then(() => {
     console.log('Postgres/Sequelize connection successfull.');
     app.listen(port, () => console.log(`APPLICATION STATUS: Listening on port ${port}`));
   }).catch(() => console.log('Unable to establish Postgres/Sequelize connection.'));
+// ==================
--- routes/api.js
+++ routes/api.js
@@ -3,18 +3,13 @@
 // ===================================
 const express = require('express');
 const router = express.Router();
-// ########################
+
+// ========================
 // Setup routes for /api
-// ########################
-module.exports = (app, db) => {
-  app.use('/api', (req, res) => {
-    app.get('/', (req, res) => {
-      res.send('Hello :)');
-    });
+// ========================
 router.get('/user', (req, res) => {
   res.send('/users GET');
   });
-};
 router.post('/user', (req, res) => {
   res.send('/user POST');
 });
