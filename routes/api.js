bd818832b326aaa8d1e92c8b61ccdff2edc92ee7
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2020-02-19 00:19:57 -0800

    full CRUD for Users

--- routes/api.js
+++ routes/api.js
@@ -8,21 +8,42 @@ const db = require('../models');
 // ========================
 // Setup routes for /api
 // ========================
-router.get('/user', (req, res) => {
-  db.User.findAll().then((users) => {
-    console.log(users);
-    res.send('/users GET');
-  });
+router.get('/users', (req, res) => {
+  db.User.findAll().then(users => res.json(users));
 });
 
-router.post('/user', (req, res) => {
+router.post('/users', (req, res) => {
   db.User.create({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password
-  }).then((user) => {
-    res.send(user);
-  });
+  }).then(user => res.json(user));
 });
+
+router.get('/users/:id', (req, res) => {
+  db.User.findAll({
+    where: {
+      id: req.params.id
+    }
+  }).then(user => res.json(user));
+});
+
+router.delete('/users/:id', (req, res) => {
+  db.User.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(() => res.json({ deleted: true }));
+});
+
+router.put('/users/:id', (req, res) => {
+  db.User.update(req.body.user, {
+    where: {
+      id: req.params.id
+    }
+  }).then(() => res.json({ updated: true }));
+});
+
+
 
 module.exports = router;
