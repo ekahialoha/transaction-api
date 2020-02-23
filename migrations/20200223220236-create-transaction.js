c7463a7d0d1c39b353a8de649c33244659b50377
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2020-02-23 14:27:13 -0800

    transaction model

---
+++ migrations/20200223220236-create-transaction.js
@@ -0,0 +1,56 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'Users',
+            key: 'id'
+          }
+        }
+      },
+      registryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'Registries',
+            key: 'id'
+          }
+        }
+      },
+      type: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      value: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Transactions');
+  }
+};
---
+++ models/transaction.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Transaction = sequelize.define('Transaction', {
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    registryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    type: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    value: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {});
+  Transaction.associate = models => {
+    models.Transaction.hasOne(models.User, {
+      foreignKey: 'userId'
+    });
+    models.Transaction.belongsTo(models.Registry, {
+      foreignKey: 'registryId'
+    });
+  };
+  return Transaction;
+};
