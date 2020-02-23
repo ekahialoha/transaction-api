192270ba01e5f9bb6e82de3bb15ac814b456c702
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2020-02-23 15:41:03 -0800

    tweaking model validations

--- models/transaction.js
+++ models/transaction.js
@@ -3,23 +3,39 @@ module.exports = (sequelize, DataTypes) => {
   const Transaction = sequelize.define('Transaction', {
     description: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: true
+      }
     },
     userId: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: true
+      }
     },
     registryId: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: true
+      }
     },
     type: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: true
+      }
     },
     value: {
       type: DataTypes.FLOAT,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isFloat: true,
+        notNull: true
+      }
     }
   }, {});
   Transaction.associate = models => {
