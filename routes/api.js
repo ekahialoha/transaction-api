e3422ca4a6165d4ef71ce293beee1d9db2e7c41f
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2020-02-23 20:09:42 -0800

    style clean up

--- routes/api.js
+++ routes/api.js
@@ -9,15 +9,10 @@ const controllers = require('../controllers')
 // =============================
 // Setup routes for /api/users
 // =============================
-console.log(controllers);
 router.get('/users', controllers.Users.findAll);
-
 router.post('/users', controllers.Users.create);
-
 router.get('/users/:id', controllers.Users.findOne);
-
 router.delete('/users/:id', controllers.Users.delete);
-
 router.put('/users/:id', controllers.Users.update);
 
 // =========================================
@@ -30,26 +25,18 @@ router.post('/sessions', controllers.Sessions.create);
 // Setup routes for /api/registries
 // ==================================
 router.get('/registries', controllers.Registries.findAll);
-
 router.post('/registries', controllers.Registries.create);
-
 router.get('/registries/:id', controllers.Registries.findOne);
-
 router.delete('/registries/:id', controllers.Registries.delete);
-
 router.put('/registries/:id', controllers.Registries.update);
 
 // =====================================
 // Setup routes for /api/transactions
 // =====================================
 router.get('/transactions', controllers.Transactions.findAll);
-
 router.post('/transactions', controllers.Transactions.create);
-
 router.get('/transactions/:id', controllers.Transactions.findOne);
-
 router.delete('/transactions/:id', controllers.Transactions.delete);
-
 router.put('/transactions/:id', controllers.Transactions.update);
 
 module.exports = router;
